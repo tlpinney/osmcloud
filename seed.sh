838b29c154a16f05d657eafe96b50425a34c8f34
Travis L Pinney <travis.pinney@gmail.com>
2013-09-14 12:47:30 -0400

    Improved the seed script

--- seed.sh
+++ seed.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+echo "127.0.1.1 master" >> /etc/hosts
+
 apt-get update -y
 apt-get install git curl vim -y
 
-cat > /etc/apt/sources.list.d/cloudera.list << EOF
-deb [arch=amd64] http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh precise-cdh4 contrib
-deb-src http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh precise-cdh4 contrib
-EOF
+cp /vagrant/media/cloudera.list /etc/apt/sources.list.d
+cp /vagrant/media/cloudera-impala.list /etc/apt/sources.list.d 
 
 curl -s http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh/archive.key | apt-key add -
-
 
 if [ ! -d /usr/local/jdk1.6.0_45 ]; then 
   cd /usr/local
@@ -24,10 +23,18 @@ export JAVA_HOME=/usr/local/jdk1.6.0_45
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/bin:/usr/local/jdk1.6.0_45/bin
 
 apt-get update -y
-apt-get install hadoop-hdfs-namenode  hadoop-hdfs-datanode -y
+apt-get install hadoop-hdfs-namenode hadoop-hdfs-datanode hadoop-0.20-mapreduce-jobtracker -y
 mkdir -p /var/lib/hadoop-hdfs/cache/hdfs/dfs/name
-cp /vagrant/media/core-site.xml /etc/hadoop/conf
-cp /vagrant/media/hdfs-site.xml /etc/hadoop/conf
+
+# copy hadoop conf files 
+cp /vagrant/media/hadoop/core-site.xml /etc/hadoop/conf
+cp /vagrant/media/hadoop/hdfs-site.xml /etc/hadoop/conf
+cp /vagrant/media/hadoop/mapred-site.xml /etc/hadoop/conf 
+
+mkdir -p /mnt/data/1/mapred/local /mnt/data/2/mapred/local /mnt/data/3/mapred/local /mnt/data/4/mapred/local
+chown -R mapred:hadoop /mnt/data/1/mapred/local /mnt/data/2/mapred/local /mnt/data/3/mapred/local /mnt/data/4/mapred/local
+
+
 chown hdfs /var/lib/hadoop-hdfs/cache/hdfs/dfs/name
 /etc/init.d/hadoop-hdfs-namenode restart
 /etc/init.d/hadoop-hdfs-datanode restart
@@ -41,6 +48,35 @@ echo "Formatting namenode"
 /etc/init.d/hadoop-hdfs-datanode restart
 mkdir -p /mnt/staging
 chown hdfs /mnt/staging
+
+# create hdfs tmp directory
+sudo -u hdfs hadoop fs -mkdir /tmp
+sudo -u hdfs hadoop fs -chmod -R 1777 /tmp
+
+# create mapreduce var directories 
+sudo -u hdfs hadoop fs -mkdir -p /var/lib/hadoop-hdfs/cache/mapred/mapred/staging
+sudo -u hdfs hadoop fs -chmod 1777 /var/lib/hadoop-hdfs/cache/mapred/mapred/staging
+sudo -u hdfs hadoop fs -chown -R mapred /var/lib/hadoop-hdfs/cache/mapred
+
+# create mapreduce system dir in hdfs 
+sudo -u hdfs hadoop fs -mkdir /tmp/mapred/system
+sudo -u hdfs hadoop fs -chown mapred:hadoop /tmp/mapred/system
+
+# start hadoop mapreduce tracker 
+sudo /etc/init.d/hadoop-0.20-mapreduce-jobtracker start
+
+# create a home directory for the mapreduce user 
+sudo -u hdfs hadoop fs -mkdir  /user/ubuntu
+sudo -u hdfs hadoop fs -chown ubuntu /user/ubuntu
+
+# set up impala and hive 
+apt-get install impala impala-shell impala-server impala-state-store hive hive-server -y
+cp /vagrant/media/impala/hive-site.xml /etc/hadoop/conf 
+
+service impala-state-store start
+service impala-server start
+
+
 
 
 exit 0
